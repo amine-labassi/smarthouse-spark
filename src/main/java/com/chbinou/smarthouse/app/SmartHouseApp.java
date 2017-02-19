b32fd5f306388d0c2763b3a7cd96c39616791249
aminelab85 <labassiamine@gmail.com>
2017-02-20 00:43:03 +0100

    add api security

--- src/main/java/com/chbinou/smarthouse/app/SmartHouseApp.java
+++ src/main/java/com/chbinou/smarthouse/app/SmartHouseApp.java
@@ -1,16 +1,20 @@
 package com.chbinou.smarthouse.app;
 
 import com.chbinou.smarthouse.app.components.IndexController;
+import com.chbinou.smarthouse.app.components.auth.AuthenticationController;
 import com.chbinou.smarthouse.app.components.lighting.LightingController;
 import com.chbinou.smarthouse.app.components.model.ElectronicInterfaceConfiguration;
 import com.chbinou.smarthouse.app.components.electronicinterface.InterfaceElectronicController;
 import com.chbinou.smarthouse.app.config.Constantes;
 import com.chbinou.smarthouse.app.config.GsonConfiguration;
-import com.chbinou.smarthouse.app.security.SecurityFilter;
+import com.chbinou.smarthouse.app.config.security.SecurityFilter;
+import com.chbinou.smarthouse.app.config.security.SmartHouseSecurityConfigFactory;
 import com.chbinou.smarthouse.app.util.ConfigurationReader;
 import com.google.gson.Gson;
 import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
+import org.pac4j.core.config.Config;
+import spark.Spark;
 
 import static spark.Spark.*;
 
@@ -19,11 +23,16 @@ import static spark.Spark.*;
  */
 public class SmartHouseApp
 {
+
     public static final GpioController gpio = GpioFactory.getInstance();
     public static ElectronicInterfaceConfiguration lightingConfigurationInstance;
 
     public static void main(String[] args) throws Exception
     {
+        //System.setProperty(org.slf4j.impl.SimpleLogger.DEFAULT_LOG_LEVEL_KEY, "INFO");
+
+        final Config config = new SmartHouseSecurityConfigFactory().build();
+
         Gson gson = GsonConfiguration.getGsonInstance();
 
         lightingConfigurationInstance = ConfigurationReader.parseConfiguration();
@@ -32,25 +41,35 @@ public class SmartHouseApp
         port(80);
         staticFiles.location("/public");
 
-        before(SecurityFilter.ensureCallSecured());
+        Spark.exception(Exception.class, (exception, request, response) -> {
+            exception.printStackTrace();
+        });
 
-        get(Constantes.Url.DEFAULT, IndexController.serveDefaultPage);
-        get(Constantes.Url.STATUS_LAMP_ALL, LightingController.getStatusAllLamp, gson::toJson);
-        get(Constantes.Url.SWITCHON_LAMP, InterfaceElectronicController.switchOnLamp, gson::toJson);
-        get(Constantes.Url.SWITCHOFF_LAMP, InterfaceElectronicController.switchOffLamp, gson::toJson);
-        get(Constantes.Url.SWITCHON_LAMP_ALL, InterfaceElectronicController.switchOnLampAll, gson::toJson);
-        get(Constantes.Url.SWITCHOFF_LAMP_ALL, InterfaceElectronicController.switchOffLampAll, gson::toJson);
-        get(Constantes.Url.SWITCHUP_WINDOW, InterfaceElectronicController.switchUPWindow, gson::toJson);
-        get(Constantes.Url.SWITCHDOWN_WINDOW, InterfaceElectronicController.switchDownWindow, gson::toJson);
-        get(Constantes.Url.SWITCHUP_WINDOW_ALL, InterfaceElectronicController.switchUpWindowAll, gson::toJson);
-        get(Constantes.Url.SWITCHDOWN_WINDOW_ALL, InterfaceElectronicController.switchDownWindowAll, gson::toJson);
-        get(Constantes.Url.STATUS_LAMP_ALL, InterfaceElectronicController.getAllClimatiseurs, gson::toJson);
-        get(Constantes.Url.SWITCHON_CLIMATISEUR, InterfaceElectronicController.switchOnClimatiseur, gson::toJson);
-        get(Constantes.Url.SWITCHOFF_CLIMATISEUR, InterfaceElectronicController.switchOffClimatiseur, gson::toJson);
-        get(Constantes.Url.SWITCHON_CLIMATISEUR, InterfaceElectronicController.switchOnClimatiseurAll, gson::toJson);
-        get(Constantes.Url.SWITCHOFF_CLIMATISEUR, InterfaceElectronicController.switchOffClimatiseurAll, gson::toJson);
+        before(Constantes.Url.LOGIN, new SecurityFilter(config, "DirectFormClient", "hsts,nosniff,noframe,xssprotection,nocache"));
+        before(Constantes.Url.API_SECURE, new SecurityFilter(config, "HeaderClient", "hsts,nosniff,noframe,xssprotection,nocache"));
+
+        // login request
+        post(Constantes.Url.LOGIN,"application/json" ,AuthenticationController.login, gson::toJson);
+
+        // secure API
+        get(Constantes.Url.DEFAULT, "application/json", IndexController.serveDefaultPage);
+        get(Constantes.Url.API_STATUS_LAMP_ALL, "application/json", LightingController.getStatusAllLamp, gson::toJson);
+        get(Constantes.Url.API_SWITCHON_LAMP, "application/json", InterfaceElectronicController.switchOnLamp, gson::toJson);
+        get(Constantes.Url.API_SWITCHOFF_LAMP, "application/json", InterfaceElectronicController.switchOffLamp, gson::toJson);
+        get(Constantes.Url.API_SWITCHON_LAMP_ALL, "application/json", InterfaceElectronicController.switchOnLampAll, gson::toJson);
+        get(Constantes.Url.API_SWITCHOFF_LAMP_ALL, "application/json", InterfaceElectronicController.switchOffLampAll, gson::toJson);
+        get(Constantes.Url.API_SWITCHUP_WINDOW, "application/json", InterfaceElectronicController.switchUPWindow, gson::toJson);
+        get(Constantes.Url.API_SWITCHDOWN_WINDOW, "application/json", InterfaceElectronicController.switchDownWindow, gson::toJson);
+        get(Constantes.Url.API_SWITCHUP_WINDOW_ALL, "application/json", InterfaceElectronicController.switchUpWindowAll, gson::toJson);
+        get(Constantes.Url.API_SWITCHDOWN_WINDOW_ALL, "application/json", InterfaceElectronicController.switchDownWindowAll, gson::toJson);
+        get(Constantes.Url.API_STATUS_LAMP_ALL, "application/json", InterfaceElectronicController.getAllClimatiseurs, gson::toJson);
+        get(Constantes.Url.API_SWITCHON_CLIMATISEUR, "application/json", InterfaceElectronicController.switchOnClimatiseur, gson::toJson);
+        get(Constantes.Url.API_SWITCHOFF_CLIMATISEUR, "application/json", InterfaceElectronicController.switchOffClimatiseur, gson::toJson);
+        get(Constantes.Url.API_SWITCHON_CLIMATISEUR, "application/json", InterfaceElectronicController.switchOnClimatiseurAll, gson::toJson);
+        get(Constantes.Url.API_SWITCHOFF_CLIMATISEUR, "application/json", InterfaceElectronicController.switchOffClimatiseurAll, gson::toJson);
+
         // 404
-        get(Constantes.Url.ANY, IndexController.notFoundResponse);
+        get(Constantes.Url.ANY, "*/*", IndexController.notFoundResponse);
 
     }
 }
---
+++ src/main/java/com/chbinou/smarthouse/app/config/security/SmartHouseSecurityConfigFactory.java
@@ -0,0 +1,48 @@
+package com.chbinou.smarthouse.app.config.security;
+
+import org.pac4j.core.authorization.authorizer.RequireAnyRoleAuthorizer;
+import org.pac4j.core.client.Clients;
+import org.pac4j.core.config.Config;
+import org.pac4j.core.config.ConfigFactory;
+import org.pac4j.core.matching.ExcludedPathMatcher;
+import org.pac4j.http.client.direct.DirectFormClient;
+import org.pac4j.http.client.direct.HeaderClient;
+import org.pac4j.http.credentials.authenticator.test.SimpleTestUsernamePasswordAuthenticator;
+import org.pac4j.jwt.config.encryption.SecretEncryptionConfiguration;
+import org.pac4j.jwt.config.signature.SecretSignatureConfiguration;
+import org.pac4j.jwt.credentials.authenticator.JwtAuthenticator;
+
+/**
+ * Created by nxuser on 18/02/2017.
+ */
+public class SmartHouseSecurityConfigFactory implements ConfigFactory
+{
+
+    public final static String KEY1 = "JaNdRgUkXp2s5v8y/B?E(H+MbPeShVmY";
+    public final static String KEY2 = "z$C&F)H@McQfTjWnZr4u7x!A%D*G-KaP";
+
+    @Override
+    public Config build()
+    {
+        DirectFormClient directFormClient = new DirectFormClient(new SimpleTestUsernamePasswordAuthenticator());
+
+        JwtAuthenticator jwtAuthenticator = new JwtAuthenticator();
+        jwtAuthenticator.setSignatureConfiguration(new SecretSignatureConfiguration(KEY1));
+        jwtAuthenticator.setEncryptionConfiguration(new SecretEncryptionConfiguration(KEY2));
+
+        HeaderClient headerClient = new HeaderClient("Authorization", "Bearer ", jwtAuthenticator);
+
+        Clients clients = new Clients(directFormClient, headerClient);
+
+        Config config = new Config(clients);
+
+        config.addAuthorizer("admin", new RequireAnyRoleAuthorizer("ROLE_SMARTHOUSE_ADMIN"));
+        config.addAuthorizer("user", new RequireAnyRoleAuthorizer("ROLE_SMARTHOUSE_USER"));
+
+        config.addMatcher("excludedPath", new ExcludedPathMatcher("^/public/*$"));
+
+        config.setHttpActionAdapter(new DefaultHttpActionAdapter());
+
+        return config;
+    }
+}
