50bc1ccf20ed8a3427b2f7337f9d46f3ebd43c1e
Yassin Chbinou <rkorkorko@outlook.fr>
2018-03-12 23:20:34 +0100

    init python app

---
+++ backend/smarthouse-api/services/WindowsManager.js
@@ -0,0 +1,25 @@
+
+
+export class Windows {
+  constructur(){
+
+  }
+
+  switchUpWindow(){
+    return new Promise((resolve, reject) => {
+
+        //algo
+        // ok
+        gpio.offPort(
+          functon(){
+
+          }, function(){
+            
+          });
+        resolve("ok");
+//algo
+      //ko
+      reject(ko);
+    });
+  }
+}
