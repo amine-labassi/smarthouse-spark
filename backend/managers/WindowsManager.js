3a15ade1728125681bcb90e7318744f2a822441f
Yassin Chbinou <rkorkorko@outlook.fr>
2018-03-15 22:04:49 +0100

    manager

--- backend/managers/WindowsManager.js
+++ backend/managers/WindowsManager.js
@@ -6,15 +6,19 @@ class WindowsManager {
     }
 
     openWindow(window) {
-        if (this.gpioAdapter.getState(window.mcpUp, window.addressUp) == true || this.gpioAdapter.getState(window.mcpDown, window.addressDown) == true) {
+        let windowIsUp = this.gpioAdapter.getState(window.mcpUp, window.addressUp);
+        let windowIsDown = this.gpioAdapter.getState(window.mcpDown, window.addressDown);
+        if (!windowIsDown || !windowIsUp) {
             this.gpioAdapter.setState(window.mcpUp, window.addressUp, true, window.upTime);
             return true;
         }
         return false;
     }
 
-    colseWindow(window) {
-        if (this.gpioAdapter.getState(window.mcpUp, window.addressUp) == true || this.gpioAdapter.getState(window.mcpDown, window.addressDown) == true) {
+    closeWindow(window) {
+        let windowIsUp = this.gpioAdapter.getState(window.mcpUp, window.addressUp);
+        let windowIsDown = this.gpioAdapter.getState(window.mcpDown, window.addressDown);
+        if (!windowIsDown || !windowIsUp) {
             this.gpioAdapter.setState(window.mcpDown, window.addressDown, true, window.downTime);
             return true;
         }
@@ -45,14 +49,55 @@ class WindowsManager {
         return true;
     }
     mouveWindow(window, pos) {
-
-
-        return 'hello';
+        let windowIsUp = this.gpioAdapter.getState(window.mcpUp, window.addressUp);
+        let windowIsDown = this.gpioAdapter.getState(window.mcpDown, window.addressDown);
+        let upTime = (window.upTime * pos)/100;
+        if (!windowIsUp || !windowIsDown) {
+            this.gpioAdapter.setState(window.mcpDown, window.addressDown, true, window.downTime)
+                .then(
+                    function (success) {
+                        this.gpioAdapter.setState(window.mcpUp, window.addressUp, upTime)
+                            .then(
+                                function (success) {},
+                                function (error) {
+                                    console.error('enable to open window ' + window.identifier);
+                                }
+                            )
+                    },
+                    function (error) {
+                        console.error('enable to close window ' + window.identifier);
+                    }
+                );
+        }
+        return !windowIsUp || !windowIsDown;
     }
 
     mouveWindowAll(pos) {
 
-        return 'hello';
+        config.forEach((elem) => {
+            elem.windows.forEach((elem) => {
+                let windowIsUp = this.gpioAdapter.getState(elem.mcpUp, elem.addressUp);
+                let windowIsDown = this.gpioAdapter.getState(elem.mcpDown, elem.addressDown);
+                let upTime = (elem.upTime * pos)/100;
+                if (!windowIsUp || !windowIsDown) {
+                    this.gpioAdapter.setState(elem.mcpDown, elem.addressDown, elem.downTime)
+                        .then(
+                            function (success) {
+                                this.gpioAdapter.setState(elem.mcpUp, elem.addressUp, upTime)
+                                    .then(
+                                        function (success) {},
+                                        function (error) {
+                                            console.error('enable to open window ' + window.identifier);
+                                        }
+                                    )
+                            },
+                            function (error) {
+                                console.error('enable to close window ' + elem.identifier);
+                            }
+                        );
+                }
+            });
+        });
     }
 
 }
