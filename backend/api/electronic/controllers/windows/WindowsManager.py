716f5d0cb3133b507fc267d16a41266c8c3fb773
amine <labassiamine@gmail.com>
2018-03-15 23:33:28 +0100

    refactor

--- backend/api/electronic/controllers/windows/WindowsManager.py
+++
@@ -1,5 +0,0 @@
-from multiprocessing import Process
-
-def setSwitchUpWindow(window):
-    print( 5)
-
--- backend/api/electronic/model/Lamp.py
+++
@@ -1,9 +0,0 @@
-class Lamp(object):
-    def __init__(self, identifier, description, status, mcpInput, mcpOutput, addressInput, addressOutput):
-        self.identifier = identifier
-        self.description = description
-        self.status = status
-        self.mcpInput = mcpInput
-        self.mcpOutput = mcpOutput
-        self.addressInput = addressInput
-        self.addressOutput = addressOutput
